09034fe78960ffe9e791857ab3469868a195f949
derabola <adu.deraj@gmail.com>
2024-02-02 11:00:23 +0000

    arrays

--- obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80d67a87eeca15ddfabc484008bf904d50d53eab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5dbf249f46a9d681af8cf8140753d2d340d7eb2")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
