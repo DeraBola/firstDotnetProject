80d67a87eeca15ddfabc484008bf904d50d53eab
derabola <adu.deraj@gmail.com>
2024-02-02 04:04:10 +0000

    ifelse statement

--- Program.cs
+++ Program.cs
@@ -1,5 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-// Console.WriteLine("Hello, C#!");
-Random dice = new Random();
-int roll = dice.Next(1, 7);
-Console.WriteLine(roll);
+﻿ Random random = new();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage;
+
+if(daysUntilExpiration <= 10) {
+    Console.WriteLine("Your subscription will expire soon. Renew now!");
+    discountPercentage = 10;
+}
+else if(daysUntilExpiration <= 5) {
+    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
+    Console.WriteLine("Renew now and save 10 %!");
+}
+else if(daysUntilExpiration == 1) {
+    Console.WriteLine("Your subscription expires within a day!");
+}
+else if(daysUntilExpiration == 0) {
+    Console.WriteLine("Your subscription has expired.");
+}
