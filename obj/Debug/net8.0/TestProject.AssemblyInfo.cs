fed293598e373b40ceafef386ba5a58a8b60903c
derabola <adu.deraj@gmail.com>
2024-02-03 11:27:10 +0000

    coinflip

--- obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14fc66a503905335e6ed7d3c5bd5f21eb62ea1dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82062aa9648cb7fa907681cb3225bda523354530")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
