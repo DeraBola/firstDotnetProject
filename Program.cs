f65d6ee5ea997f77ce1f83d5d40302b72e286d47
derabola <adu.deraj@gmail.com>
2024-02-08 16:34:00 +0000

    data type casting and conversion

--- Program.cs
+++ Program.cs
@@ -1,26 +1,4 @@
-﻿ for (int i = 1; i <= 100; i++)
-{
-    int count = i;
-
-    if ((count % 5 == 0 ) && (count % 3 == 0))
-        Console.WriteLine($"{count} - FizzBuzz");
-    else if (count % 3 == 0)
-        Console.WriteLine($"{count} - Fizz");
-    else if (count % 5 == 0)
-        Console.WriteLine($"{count} - Buzz");
-    else
-        Console.WriteLine($"{count}");
-}
-
-
-// for (int i = 1; i < 101; i++)
-// {
-//     if ((i % 3 == 0) && (i % 5 == 0))
-//         Console.WriteLine($"{i} - FizzBuzz");
-//     else if (i % 3 == 0)
-//         Console.WriteLine($"{i} - Fizz");
-//     else if (i % 5 == 0)
-//         Console.WriteLine($"{i} - Buzz");
-//     else
-//         Console.WriteLine($"{i}");
-// }
+﻿int first = 2;
+string second = "4";
+string result = first + second;
+Console.WriteLine(result);
--- obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f97ed2df58a235c9bec4c65f834b90648cc84e74")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c89c799e27c0217111c85511829e898de777ccb")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
