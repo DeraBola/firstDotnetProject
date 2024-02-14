66b4a4c6fc1d092257181bda97a9b1e6dfce1082
derabola <adu.deraj@gmail.com>
2024-02-14 11:45:39 +0000

    Formatting

--- Program.cs
+++ Program.cs
@@ -1,12 +1,2 @@
-﻿string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
-string[] orderID = orderStream.Split(',');
-Array.Sort(orderID);
-
-foreach(var item in orderID){
-    if(item.Length == 4){
-        
-        Console.WriteLine(item);
-    }else{
-         Console.WriteLine(item + "\t- Error");
-    }
-}
+﻿string input = "Pad this";
+ Console.WriteLine(input.PadLeft(12));
--- obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f56abeac2891bc7970c3e3aa837f089b89c103d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8dc777e5a9aa4b26f18b750b8539f8238a15da50")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
